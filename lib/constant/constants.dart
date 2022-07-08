b7fb53753d554131343032e37f560cd4a7fd0a76
Spandan Ghosh <spandu500@gmail.com>
2022-07-08 20:04:19 +0530

    local Bs

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,13 +1,13 @@
 String mapBoxApiKey =
-    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+    'AIzaSyBlT6lxObRy2hEFm0IVc4QfH3H72TOe3bc';
 
 String nftStorageApiKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBBODI5QTE0OTA4NUY3MTA0NTRDOEQ3RTM5RUEzMGJhMmMzY0RhNTgiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1NzI4OTI3MTU4MSwibmFtZSI6Im1ham9yIn0.3clRVZr144PWBqoiHUw7tAq91z07gWOs3bLXohqpdfI';
 
 const String rpcUrl =
-    "https://ropsten.infura.io/v3/e43345b7383246378963be7acd5b6c67";
+    "HTTP://127.0.0.1:7545";
 //"http://127.0.0.1:7545";
 
-const String contractAddress = "0x702058Ba021Cd4e4F847B40F32b58aA5be3a4661";
+const String contractAddress = "0x9f117E66E192D7568DAb144DD10c7b7683F47182";
 
-const int chainId = 3; //Ropsten-3, Polygon testnet-80001, local ganache-1337
+const int chainId = 1337; //Ropsten-3, Polygon testnet-80001, local ganache-1337
